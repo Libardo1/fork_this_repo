efa1dc8da48bfca2cb4eece2dec747d391dd470f
sk6aus6 <sk6aus6@gmail.com>
2015-06-16 22:34:35 +1000

    manual colour fill

--- Lets_Plot_Some_Data.R
+++ Lets_Plot_Some_Data.R
@@ -18,7 +18,7 @@
 
 library(ggplot2)
 
-p1 <- ggplot(aes(x = cut, fill = cut), data = diamonds) + geom_bar(width = 1)
+p1 <- ggplot(aes(x = cut, fill = cut), data = diamonds) + geom_bar(width = 1, colour = 'darkgreen')
 
 # Executing the named plot object produces a bar chart:
 
@@ -26,8 +26,23 @@ p1
 
 # Changing the coordinate system produces a coxcomb plot:
 
-p1 + coord_polar()
+our.pal <- function(x){
+        return(paste(c('#edf8fb', '#ccece6', '#99d8c9', '#66c2a4', '#2ca25f')[x]))
+}
 
+our.pal(1)
+p1 + coord_polar() + scale_fill_manual(values = c('#ccece6', '#99d8c9', '#66c2a4', '#2ca25f','#006d2c'))
+        
+
+#                                              #006d2c)
+
+p2 <- ggplot(aes(x = carat), data = diamonds)
+p2 + geom_histogram(aes(fill = cut)) + scale_fill_manual(values = c('#a6611a',
+        '#dfc27d',
+        '#f5f5f5',
+        '#80cdc1',
+        '#018571'))
+p2 + geom_histogram() 
 # Please edit the above code to change something about the plot 'p1' or add some
 # code to make a new plot then save your changes to your copy of this file.
 # Next commit these changes to your fork and send me a pull request.
@@ -37,3 +52,7 @@ p1 + coord_polar()
 # repository
 # <https://help.github.com/articles/merging-a-pull-request/>
 # Ok over to you!
+
+
+
+
