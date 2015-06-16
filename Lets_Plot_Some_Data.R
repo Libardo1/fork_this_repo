3d60f4b5373ee8c89e2035ddfa5ce194ab5aabd9
Ben R Fitzpatrick <ben.r.fitzpatrick@gmail.com>
2015-06-17 09:25:11 +1000

    Reveiwed changes from sk6aus6-master fork and implemented the promising ones.

--- Lets_Plot_Some_Data.R
+++ Lets_Plot_Some_Data.R
@@ -26,23 +26,17 @@ p1
 
 # Changing the coordinate system produces a coxcomb plot:
 
-our.pal <- function(x){
-        return(paste(c('#edf8fb', '#ccece6', '#99d8c9', '#66c2a4', '#2ca25f')[x]))
-}
+p1 + coord_polar()
 
-our.pal(1)
+# Using HEX colour codes in a manual scale:
+
 p1 + coord_polar() + scale_fill_manual(values = c('#ccece6', '#99d8c9', '#66c2a4', '#2ca25f','#006d2c'))
-        
 
-#                                              #006d2c)
+# Creating a second plot to practise using HEX colour codes in a manual scale:
 
 p2 <- ggplot(aes(x = carat), data = diamonds)
-p2 + geom_histogram(aes(fill = cut)) + scale_fill_manual(values = c('#a6611a',
-        '#dfc27d',
-        '#f5f5f5',
-        '#80cdc1',
-        '#018571'))
-p2 + geom_histogram() 
+p2 + geom_histogram(aes(fill = cut)) + scale_fill_manual(values = c('#a6611a', '#dfc27d', '#f5f5f5', '#80cdc1', '#018571')) + theme(panel.background = element_rect(fill = 'black'))
+
 # Please edit the above code to change something about the plot 'p1' or add some
 # code to make a new plot then save your changes to your copy of this file.
 # Next commit these changes to your fork and send me a pull request.
