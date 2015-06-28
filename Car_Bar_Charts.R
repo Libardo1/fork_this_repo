8014db500f1e8d11eb4e4fe7ec87c3322e01be95
Ben R Fitzpatrick <ben.r.fitzpatrick@gmail.com>
2015-06-29 09:55:24 +1000

    Added new polar coordinate plot.

--- Car_Bar_Charts.R
+++ Car_Bar_Charts.R
@@ -2,3 +2,9 @@ library('ggplot2')
 p <- ggplot(mtcars, aes(factor(cyl)))
 p <- p + geom_bar(aes(fill = factor(cyl))) 
 p + coord_polar()
+
+# New plots
+
+p + geom_bar(aes(fill = factor(cyl)))
+
+p + geom_bar(aes(fill = factor(cyl))) + coord_polar(theta = 'y')
